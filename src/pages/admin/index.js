603e7a7777755ed4292026b87d7bdbdd5eab4aa1
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-08-31 19:07:26 +0900

    mailとpasswordを受け取れるようになった

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import Logo from '../../assets/img/geoHoop_01.png'
 
 const Index = () => {
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
   const [isValidationClear, setValidation] = useState(false)
   const style = {
     btnStyle: {
@@ -24,11 +26,11 @@ const Index = () => {
         <div className="box">
           <div className="input-place">
             <p>メールアドレス</p>
-            <input type="email" />
+            <input type="email" onChange={(e) => setEmail(e.target.value)}/>
           </div>
           <div className="input-place">
             <p>パスワード</p>
-            <input type="password" />
+            <input type="password" onChange={(e) => setPassword(e.target.value)}/>
           </div>
         </div>
         <button
