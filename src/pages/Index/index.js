dd957811d4c38a15a926dea53d2d00df66716a91
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-01 20:56:56 +0900

    検索用のformを追加

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -2,10 +2,12 @@ import React, {useState} from 'react'
 import './style.scss'
 import Header from '../../component/Header/'
 import { BluePalm, Court, FenceBuildings, GlassBoard, Sunset, YellowPaint } from '../../assets/img/'
-import { GoogleIcon } from '../../assets/icons'
+import { GoogleIcon, SearchIcon } from '../../assets/icons'
+import Prefecture from '../../assets/json/prefectureTest.json'
 
 const Index = () => {
   const [isDialogOpen, setDialog] = useState(false)
+  const [isOnFocus, setOnFocus] = useState(false)
   const [isLoginForm, setLoginForm] = useState(true)
   const [loginEmail, setLoginEmail] = useState(null)
   const [loginPassword, setLoginPassword] = useState(null)
@@ -55,37 +57,59 @@ const Index = () => {
         setLoginForm={() => setDialog(!isDialogOpen)}
       />
       <main style={style.mainStyle}>
-        {isDialogOpen &&
+        {!isDialogOpen ?
+          <div className="search-box">
+            <div className="search-form">
+              <input
+                type="text"
+                placeholder="we all equal on the court."
+                onFocus={() => setOnFocus(!isOnFocus)}
+              />
+              <SearchIcon />
+            </div>
+            {isOnFocus &&
+              <div className="search-suggest">
+                {Prefecture.map((v) => {
+                  return (
+                    <div className="region-box">
+                      <li>{v.name}</li>
+                    </div>
+                  )
+                })}
+              </div>
+            }
+          </div>
+          :
           <form className="login-form">
-          <p className="login-title">{isLoginForm ? 'ログイン' : 'アカウント登録'}</p>
-          <div className="box">
-            <div className="input-place">
-              <p>メールアドレス</p>
-              {isLoginForm ?
-                <input type="email" onChange={(e) => checkLoginEmail(e.target.value)} />:
-                <input type="email" onChange={(e) => checkRegisterEmail(e.target.value)} />
-              }
+            <p className="login-title">{isLoginForm ? 'ログイン' : 'アカウント登録'}</p>
+            <div className="box">
+              <div className="input-place">
+                <p>メールアドレス</p>
+                {isLoginForm ?
+                  <input type="email" onChange={(e) => checkLoginEmail(e.target.value)} />:
+                  <input type="email" onChange={(e) => checkRegisterEmail(e.target.value)} />
+                }
+              </div>
+              <div className="input-place">
+                <p>パスワード</p>
+                {isLoginForm ?
+                  <input type="password" onChange={(e) => checkLoginPassword(e.target.value)} />:
+                  <input type="password" onChange={(e) => checkRegisterPassword(e.target.value)} />
+                }
+              </div>
             </div>
-            <div className="input-place">
-              <p>パスワード</p>
-              {isLoginForm ?
-                <input type="password" onChange={(e) => checkLoginPassword(e.target.value)} />:
-                <input type="password" onChange={(e) => checkRegisterPassword(e.target.value)} />
-              }
+            <div className="google-login">
+              <GoogleIcon />
             </div>
-          </div>
-          <div className="google-login">
-            <GoogleIcon />
-          </div>
-          {isLoginForm ?
-            <button>ログイン</button>:
-            <button>アカウント作成</button>
-          }
-          <p className="switch-method">
-            {isLoginForm ? '新規ユーザは' : 'ログインは'}
-            <span onClick={() => setLoginForm(!isLoginForm)}>こちら</span>
-          </p>
-        </form>
+            {isLoginForm ?
+              <button>ログイン</button>:
+              <button>アカウント作成</button>
+            }
+            <p className="switch-method">
+              {isLoginForm ? '新規ユーザは' : 'ログインは'}
+              <span onClick={() => setLoginForm(!isLoginForm)}>こちら</span>
+            </p>
+          </form>
         }
       </main>
       <div className="recommend">
