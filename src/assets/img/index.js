c79b06f033a8cfc4688eb9ab0029da9d8409a99a
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-01 19:26:27 +0900

    LP用背景画像

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,11 +1,19 @@
 import Avatar from './avatar.png'
+import BluePalm from './bluePalm.jpg'
 import Court from './court.jpeg'
-import Sunset from './sunset.jpg'
+import FenceBuildings from './fenceBuildings.jpg'
+import GlassBoard from './glassBoard.jpg'
 import Noimg from './noimg.png'
+import Sunset from './sunset.jpg'
+import YellowPaint from './yellowPaint.jpg'
 
 export {
   Avatar,
+  BluePalm,
   Court,
+  FenceBuildings,
+  GlassBoard,
   Sunset,
-  Noimg
+  Noimg,
+  YellowPaint
 }
