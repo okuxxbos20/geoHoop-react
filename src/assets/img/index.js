e6092a9ca99d802e49d4d7b57c48eae6d5c2a25a
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-01 09:40:29 +0900

    assets/imgのモジュール

---
+++ src/assets/img/index.js
@@ -0,0 +1,11 @@
+import Avatar from './avatar.png'
+import Court from './court.jpeg'
+import Sunset from './sunset.jpg'
+import Noimg from './noimg.png'
+
+export {
+  Avatar,
+  Court,
+  Sunset,
+  Noimg
+}
