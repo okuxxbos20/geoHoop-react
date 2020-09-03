30c49bac74771680134bcf12a844aa532360a22e
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-03 17:21:04 +0900

    fill-rule -> fillRule

--- src/assets/icons/ArrowRightIcon.js
+++ src/assets/icons/ArrowRightIcon.js
@@ -8,7 +8,7 @@ const ArrowRightIcon = () => {
   return (
     <div style={style}>
       <svg viewBox="0 0 16 16" className="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+        <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
       </svg>
     </div>
   )
