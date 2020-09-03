2490c64815ab08e6b56d182890833cb685388127
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-03 18:30:36 +0900

    loginとregisterのコンポーネント化完了

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -2,8 +2,9 @@ import React, {useState, useEffect} from 'react'
 import './style.scss'
 import Header from '../../component/Header/'
 import { BluePalm, Court, FenceBuildings, GlassBoard, Sunset, YellowPaint } from '../../assets/img/'
-import { GoogleIcon, SearchIcon } from '../../assets/icons'
+import { SearchIcon } from '../../assets/icons'
 import Prefecture from '../../assets/json/prefectureTest.json'
+import Login from './login'
 
 const Index = () => {
   const [isDialogOpen, setDialog] = useState(true)
@@ -16,88 +17,18 @@ const Index = () => {
     setRunNum(Math.floor(Math.random() * randomImg.length))
   }, [randomImg.length])　
 
-  // login
-  const [loginInfo, setLoginInfo] = useState({ email: '', password: '' })
-  const inputLoginInfo = (e) => {
-    setLoginInfo({ ...loginInfo, [e.target.name]: e.target.value })
-  }
-
-  // register
-  const [registerInfo, setRegisterInfo] = useState({ email: '', password: '' })
-  const inputRegisterInfo = (e) => {
-    setRegisterInfo({ ...registerInfo, [e.target.name]: e.target.value })
-  }
-
-  // validation
-  const validation = () => {
-    let loginErr = {}
-    let registerErr = {}
-
-    if (isLoginForm) {
-      if (loginInfo.email === '') {
-        loginErr.email = '*メールアドレスを入力してください'
-      }
-      if (loginInfo.password === '') {
-        loginErr.password = '*パスワードを入力してください'
-      }
-    } else {
-      if (registerInfo.email === '') {
-        registerErr.email = '*メールアドレスを入力してください'
-      }
-      if (registerInfo.password === '') {
-        registerErr.password = '*パスワードを入力してください'
-      }
-    }
-
-    return { loginErr, registerErr }
-  }
-
-  // showErr
-  const showErr = (errObj) => {
-    let errMsg = ''
-    for (let err in errObj) {
-      errMsg += `${errObj[err]}. `
-    }
-    alert(`Errors ${errMsg}`)
-  }
-
-  // submit
-  const submitInfo = (e) => {
-    e.preventDefault()
-    const errs = validation()
-
-    if (isLoginForm) {
-      if (Object.keys(errs.loginErr).length === 0) {
-        // ここでfirebaseにemailとpassを投げる
-        alert(`loginInfo: ${loginInfo.email}, ${loginInfo.password}`)
-        setLoginInfo({ email: '', password: '' })
-      } else {
-        showErr(errs.loginErr)
-      }
-    } else {
-      if (Object.keys(errs.registerErr).length === 0) {
-        // ここでfirebaseにemailとpassを投げる
-        alert(`registerInfo: ${registerInfo.email}, ${registerInfo.password}`)
-        setRegisterInfo({ email: '', password: '' })
-      } else {
-        showErr(errs.registerErr)
-      }
-    }
-  }
 
   const style = {
     mainStyle : {
       backgroundImage: `url(${randomImg[ranNum]})`
     }
   }
-  console.log(loginInfo)
-  console.log(registerInfo)
   return (
     <div>
       <Header
         color='#e8ecf1'
         background='transparent'
-        setLoginForm={() => setDialog(!isDialogOpen)}
+        setDialog={() => setDialog(!isDialogOpen)}
       />
       <main style={style.mainStyle}>
         {!isDialogOpen ?
@@ -123,43 +54,10 @@ const Index = () => {
             }
           </div>
           :
-          <form className="login-form" onSubmit={submitInfo}>
-            <p className="login-title">{isLoginForm ? 'ログイン' : 'アカウント登録'}</p>
-            <div className="box">
-              <div className="input-place">
-                <p>メールアドレス</p>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="email"
-                  value={isLoginForm ? loginInfo.email : registerInfo.email}
-                  onChange={isLoginForm ? inputLoginInfo : inputRegisterInfo}
-                  autoFocus
-                />
-              </div>
-              <div className="input-place">
-                <p>パスワード</p>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="password"
-                  value={isLoginForm ? loginInfo.password : registerInfo.password}
-                  onChange={isLoginForm ? inputLoginInfo : inputRegisterInfo}
-                />
-              </div>
-            </div>
-            <div className="google-login">
-              <GoogleIcon />
-            </div>
-            {isLoginForm ?
-              <button type="submit">ログイン</button>:
-              <button type="submit">アカウント作成</button>
-            }
-            <p className="switch-method">
-              {isLoginForm ? '新規ユーザは' : 'ログインは'}
-              <span onClick={() => setLoginForm(!isLoginForm)}>こちら</span>
-            </p>
-          </form>
+          <Login
+            isLoginForm={isLoginForm}
+            setLoginForm={() => setLoginForm(!isLoginForm)}
+          />
         }
       </main>
       <div className="recommend">
--- src/pages/Index/login.js
+++ src/pages/Index/login.js
@@ -1,9 +1,116 @@
-import React from 'react'
-import { GoogleIcon, SearchIcon } from '../../assets/icons'
+import React, {useState} from 'react'
+import { GoogleIcon } from '../../assets/icons'
+import './style.scss'
 
-const Login = () => {
+const Login = (props) => {
+  const isLoginForm = props.isLoginForm
+  // login
+  const [loginInfo, setLoginInfo] = useState({ email: '', password: '' })
+  const inputLoginInfo = (e) => {
+    setLoginInfo({ ...loginInfo, [e.target.name]: e.target.value })
+  }
+
+  // register
+  const [registerInfo, setRegisterInfo] = useState({ email: '', password: '' })
+  const inputRegisterInfo = (e) => {
+    setRegisterInfo({ ...registerInfo, [e.target.name]: e.target.value })
+  }
+
+  // validation
+  const validation = () => {
+    let loginErr = {}
+    let registerErr = {}
+
+    if (isLoginForm) {
+      if (loginInfo.email === '') {
+        loginErr.email = '*メールアドレスを入力してください'
+      }
+      if (loginInfo.password === '') {
+        loginErr.password = '*パスワードを入力してください'
+      }
+    } else {
+      if (registerInfo.email === '') {
+        registerErr.email = '*メールアドレスを入力してください'
+      }
+      if (registerInfo.password === '') {
+        registerErr.password = '*パスワードを入力してください'
+      }
+    }
+
+    return { loginErr, registerErr }
+  }
+
+  // showErr
+  const showErr = (errObj) => {
+    let errMsg = ''
+    for (let err in errObj) {
+      errMsg += `${errObj[err]}. `
+    }
+    alert(`Errors ${errMsg}`)
+  }
+
+  // submit
+  const submitInfo = (e) => {
+    e.preventDefault()
+    const errs = validation()
+
+    if (isLoginForm) {
+      if (Object.keys(errs.loginErr).length === 0) {
+        // ここでfirebaseにemailとpassを投げる
+        alert(`loginInfo: ${loginInfo.email}, ${loginInfo.password}`)
+        setLoginInfo({ email: '', password: '' })
+      } else {
+        showErr(errs.loginErr)
+      }
+    } else {
+      if (Object.keys(errs.registerErr).length === 0) {
+        // ここでfirebaseにemailとpassを投げる
+        alert(`registerInfo: ${registerInfo.email}, ${registerInfo.password}`)
+        setRegisterInfo({ email: '', password: '' })
+      } else {
+        showErr(errs.registerErr)
+      }
+    }
+  }
+
   return (
-
+    <form className="login-form" onSubmit={submitInfo}>
+    <p className="login-title">{isLoginForm ? 'ログイン' : 'アカウント登録'}</p>
+    <div className="box">
+      <div className="input-place">
+        <p>メールアドレス</p>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          value={isLoginForm ? loginInfo.email : registerInfo.email}
+          onChange={isLoginForm ? inputLoginInfo : inputRegisterInfo}
+          autoFocus
+        />
+      </div>
+      <div className="input-place">
+        <p>パスワード</p>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={isLoginForm ? loginInfo.password : registerInfo.password}
+          onChange={isLoginForm ? inputLoginInfo : inputRegisterInfo}
+        />
+      </div>
+    </div>
+    <div className="google-login">
+      <GoogleIcon />
+    </div>
+    {isLoginForm ?
+      <button type="submit">ログイン</button>:
+      <button type="submit">アカウント作成</button>
+    }
+    <p className="switch-method">
+      {isLoginForm ? '新規ユーザは' : 'ログインは'}
+      <span onClick={() => props.setLoginForm()}>こちら</span>
+    </p>
+  </form>
   )
 }
 
