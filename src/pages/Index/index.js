cbf950a19ae79be6db61f7b5e1596772de69ca01
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-03 18:56:17 +0900

    検索画面をコンポーネント化した

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,10 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import './style.scss'
 import Header from '../../component/Header/'
-import { BluePalm, Court, FenceBuildings, GlassBoard, Sunset, YellowPaint } from '../../assets/img/'
-import { SearchIcon } from '../../assets/icons'
-import Prefecture from '../../assets/json/prefectureTest.json'
 import Login from './login'
+import Search from './search'
+import { BluePalm, Court, FenceBuildings, GlassBoard, Sunset, YellowPaint } from '../../assets/img/'
 
 const Index = () => {
   const [isDialogOpen, setDialog] = useState(true)
@@ -16,7 +15,6 @@ const Index = () => {
   useEffect(() => {
     setRunNum(Math.floor(Math.random() * randomImg.length))
   }, [randomImg.length])　
-
 
   const style = {
     mainStyle : {
@@ -32,27 +30,10 @@ const Index = () => {
       />
       <main style={style.mainStyle}>
         {!isDialogOpen ?
-          <div className="search-box">
-            <div className="search-form">
-              <input
-                type="text"
-                placeholder="we all equal on the court."
-                onFocus={() => setOnFocus(!isOnFocus)}
-              />
-              <SearchIcon />
-            </div>
-            {isOnFocus &&
-              <div className="search-suggest">
-                {Prefecture.map((v) => {
-                  return (
-                    <div className="region-box">
-                      <li>{v.name}</li>
-                    </div>
-                  )
-                })}
-              </div>
-            }
-          </div>
+          <Search
+            isOnFocus={isOnFocus}
+            setOnFocus={() => setOnFocus(!isOnFocus)}
+          />
           :
           <Login
             isLoginForm={isLoginForm}
