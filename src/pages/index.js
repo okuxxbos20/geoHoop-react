f51e0d4be4c0057379abc873740a0e37c6639801
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-08 23:44:40 +0900

    各コンポーネントのpathはここで管理する

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export {default as Admin} from './admin'
+export {default as Court} from './court'
+export {default as Index} from './index/'
+export {default as Profile} from './profile'
+export {default as Dashboard} from './admin/Dashboard'
