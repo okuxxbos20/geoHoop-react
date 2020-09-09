503aa8b9addcf798d2c9d3d5308da194b3956567
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-09 18:12:49 +0900

    redux-thunk導入

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -4,6 +4,7 @@ import {
   applyMiddleware
 } from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
+import thunk from 'redux-thunk'
 import { UsersReducer } from '../users/reducers'
 
 const createStore = (history) => {
@@ -13,7 +14,8 @@ const createStore = (history) => {
       users: UsersReducer
     }),
     applyMiddleware(
-      routerMiddleware(history)
+      routerMiddleware(history),
+      thunk
     )
   )
 }
