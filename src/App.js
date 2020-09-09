14402834025a606c07223bd595ef0fba86f5200a
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-09 22:21:13 +0900

    不要なコメントアウトを削除

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Router from './Router'
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.scss'
-// import { Admin, Court, Index, Profile, Dashboard } from './pages'
 
 const App = () => {
   return (
