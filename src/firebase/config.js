2a64844818ff864b8ce408fd0b0cb03adeab6d14
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-09 19:39:47 +0900

    firebase導入

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4gG108KhOPfqe-DS89xonVAFAsLbPvgk",
+  authDomain: "geohoop-f935c.firebaseapp.com",
+  databaseURL: "https://geohoop-f935c.firebaseio.com",
+  projectId: "geohoop-f935c",
+  storageBucket: "geohoop-f935c.appspot.com",
+  messagingSenderId: "738514969696",
+  appId: "1:738514969696:web:a3a2ed28976c657d45cf05",
+  measurementId: "G-2VJ2QFBMH8"
+};
