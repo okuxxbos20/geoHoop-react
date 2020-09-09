75f3cfb0c584426f9a9fb990369f7b17bea2c1f0
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-09 19:14:43 +0900

    参照をSIGN_IN -> LOG_IN

--- src/redux/users/reducers.js
+++ src/redux/users/reducers.js
@@ -3,7 +3,7 @@ import initialState from '../store/initialState'
 
 export const UsersReducer = (state = initialState.users, action) => {
   switch (action.type) {
-    case Actions.SIGN_IN:
+    case Actions.LOG_IN:
       return {
         ...state,
         ...action.payload
