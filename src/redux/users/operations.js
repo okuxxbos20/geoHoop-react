8efbe8d9bdef3fd3bcd9b15de64f11679c9edec2
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-09 22:22:28 +0900

    アカウント登録処理を実装

--- src/redux/users/operations.js
+++ src/redux/users/operations.js
@@ -1,5 +1,5 @@
-// import { auth } from "firebase"
 // import { LoginAction } from './actions'
+import { auth, db, FirebaseTimestamp } from '../../firebase/index'
 
 // export const login = (email, password) => {
 //   return async (dispatch, getState) => {
@@ -22,3 +22,37 @@
 //     }
 //   }
 // }
+
+export const register = (email, password) => {
+  return async (dispatch) => {
+    if (email === '' || password === '') {
+      alert('必須項目が未入力です')
+      return false
+    }
+    return auth.createUserWithEmailAndPassword(email, password).then((result) => {
+      const user = result.user
+      if (user) {
+        const uid = user.uid
+        const timestamp = FirebaseTimestamp.now()
+        const userInitialData = {
+          bookmarks: [],
+          createdAt: timestamp,
+          email: email,
+          img: '',
+          lastLogin: timestamp,
+          likes: [],
+          uid: uid,
+          isAdmin: false,
+          updatedAt: timestamp,
+          name: '',
+        }
+
+        db.collection('users').doc(uid).set(userInitialData).then(() => {
+          console.log('success to register!')
+        }).catch((err) => {
+          alert(err)
+        })
+      }
+    })
+  }
+}
