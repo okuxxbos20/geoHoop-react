85a42fbb9232bc6919c6fdbff89c1d3c9750c02b
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-09-15 21:43:38 +0900

    propsでサイズと色を指定可能になった

--- src/assets/icons/CameraIcon.js
+++ src/assets/icons/CameraIcon.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const CameraIcon = () => {
+const CameraIcon = (props) => {
   const style = {
-    width: '25px',
-    height: '25px',
+    color: props.color,
+    width: props.width,
+    height: props.height
   }
   return (
     <div style={style}>
-      <svg viewBox="0 0 16 16" className="bi bi-camera-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-        <path fillRule="evenodd" d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"/>
+      <svg viewBox="0 0 16 16" className="bi bi-camera" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" d="M15 12V6a1 1 0 0 0-1-1h-1.172a3 3 0 0 1-2.12-.879l-.83-.828A1 1 0 0 0 9.173 3H6.828a1 1 0 0 0-.707.293l-.828.828A3 3 0 0 1 3.172 5H2a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z"/>
+        <path fillRule="evenodd" d="M8 11a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+        <path d="M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
       </svg>
     </div>
   )
